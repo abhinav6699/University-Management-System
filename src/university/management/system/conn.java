123836e50a8c33c46cbcbb1f431902fb553c478f
Kumar Abhinav <kmrabhinav100@gmail.com>
2021-09-23 23:07:49 +0530

    Add files via upload

---
+++ src/university/management/system/conn.java
@@ -0,0 +1,22 @@
+
+package university.management.system;
+import java.sql.*;
+/**
+ *
+ * @author nikit
+ */
+public class conn {
+    Connection c;
+    Statement s;
+    public conn(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c= DriverManager.getConnection("jdbc:mysql:///ums","root","");
+            s= c.createStatement();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
+}
